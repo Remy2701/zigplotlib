cfd12347212b5f9bad298db2c0d992d4f7d3281a
remy <remy.forjanic@gmail.com>
2024-01-03 18:46:21 +0100

    Added imports in root

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,25 @@
 const std = @import("std");
-const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+// Plot Module
+pub const Figure = @import("plot/Figure.zig");
+pub const Plot = @import("plot/Plot.zig");
+pub const Line = @import("plot/Line.zig");
+pub const Area = @import("plot/Area.zig");
+pub const Scatter = @import("plot/Scatter.zig");
+pub const FigureInfo = @import("plot/FigureInfo.zig");
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+// Util Module
+pub const Range = @import("util/range.zig").Range;
+pub const polyshape = @import("util/polyshape.zig");
+
+// SVG Module
+const SVG = @import("svg/SVG.zig");
+const length = @import("svg/util/length.zig");
+const LengthPercent = length.LengthPercent;
+const LengthPercentAuto = length.LengthPercentAuto;
+pub const rgb = @import("svg/util/rgb.zig");
+pub const RGB = rgb.RGB;
+
+test "Plot Test" {
+    std.testing.refAllDecls(FigureInfo);
 }
